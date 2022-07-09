688a428ed6a3f17eacd3e1a46f47cfa7a81617c5
Lukas <lukas.petersson.1999@gmail.com>
2022-07-09 22:05:53 +0200

    more cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,8 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
+          footer
       </footer>
     </div>
   )
